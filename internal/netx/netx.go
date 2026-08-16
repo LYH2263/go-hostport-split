61aebf0cf751076073b98cf515b61ff7f5e4c19d
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-17 00:03:09 +0800

    fix: IPv6 hostport 拆错

--- internal/netx/netx.go
+++ internal/netx/netx.go
@@ -1,14 +1,7 @@
 package netx
 
-import (
-	"fmt"
-	"strings"
-)
+import "net"
 
 func Split(hostport string) (host, port string, err error) {
-	i := strings.IndexByte(hostport, ':') // BUG: first colon
-	if i < 0 {
-		return "", "", fmt.Errorf("missing port")
-	}
-	return hostport[:i], hostport[i+1:], nil
+	return net.SplitHostPort(hostport)
 }
